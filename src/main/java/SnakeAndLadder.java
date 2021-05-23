863fe147edc269800b09638038982d3662af0eeb
Durgaprasath01415 <sdp8754262828@gmail.com>
2021-05-23 12:18:52 +0530

    ADDED:The inital position of player

--- src/main/java/SnakeAndLadder.java
+++ src/main/java/SnakeAndLadder.java
@@ -1,5 +1,7 @@
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to the simple Snake & Ladder Game");
+        int player_position = 0;
+        System.out.println(" Starting position of player : "+player_position);
     }
 }
