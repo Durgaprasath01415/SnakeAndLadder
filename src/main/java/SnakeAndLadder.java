5c0c3b3493b64087a8eff3f9a4848f25540ab0e0
Durgaprasath01415 <sdp8754262828@gmail.com>
2021-05-23 12:24:30 +0530

    ADDED:Random to thrown a die and get value from 1 to 6

--- src/main/java/SnakeAndLadder.java
+++ src/main/java/SnakeAndLadder.java
@@ -3,5 +3,7 @@ public class SnakeAndLadder {
         System.out.println("Welcome to the simple Snake & Ladder Game");
         int player_position = 0;
         System.out.println(" Starting position of player : "+player_position);
+        int random = (int)(Math.random()*6) + 1;
+        System.out.println(" Random variables from 1 - 6 : "+random);
     }
 }
